7000f9e58ab9345e6599d3c162e6eb91ed2a7627
caroWD <juego.fuego@gmail.com>
2026-05-19 16:51:04 -0500

    feat(packages/backend): add cors and error middlewares

--- packages/backend/src/config/config.ts
+++ packages/backend/src/config/config.ts
@@ -4,4 +4,5 @@ export const {
   NODE_ENV = 'development',
   PORT = '3000',
   DB_FILE_NAME = 'file:local.db',
+  ACCEPTED_ORIGINS = 'http://localhost:3000',
 } = process.env
--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,15 +1,18 @@
 import express from 'express'
-import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import { PORT } from './config/config.ts'
+import { corsMiddleware } from './middlewares/corsMiddleware.ts'
+import { errorMiddleware } from './middlewares/errorMiddleware.ts'
 
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(corsMiddleware())
 app.use(cookieParser())
 
 app.get('/', (_req, res) => res.send('Hello World!'))
+
+app.use(errorMiddleware)
 
 app.listen(PORT, () =>
   console.log(`Server listening at http://localhost:${PORT}`)
