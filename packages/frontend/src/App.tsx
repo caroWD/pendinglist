d39dbed6ec9e6d11d5e9a78c34e39da0254ed466
caroWD <juego.fuego@gmail.com>
2026-05-20 00:31:36 -0500

    feat(packages/frontend): add tooltip provider

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,14 +1,17 @@
 import { Button } from '@/components/ui/button'
 import { DarkModeProvider } from './contexts/DarkModeProvider'
 import { DarkModeToggle } from './components/globals/DarkModeToggle'
+import { TooltipProvider } from './components/ui/tooltip'
 
 function App() {
   return (
     <DarkModeProvider defaultDarkMode="dark" storageKey="dark-mode">
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button>Click me</Button>
-        <DarkModeToggle />
-      </div>
+      <TooltipProvider>
+        <div className="flex min-h-svh flex-col items-center justify-center">
+          <Button>Click me</Button>
+          <DarkModeToggle />
+        </div>
+      </TooltipProvider>
     </DarkModeProvider>
   )
 }
