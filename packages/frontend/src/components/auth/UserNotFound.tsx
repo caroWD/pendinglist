03ab805d92280ad2a47b1acf1bb30aac1dc87628
caroWD <juego.fuego@gmail.com>
2026-05-20 23:58:05 -0500

    feat(packages/frontend): add user not found page

---
+++ packages/frontend/src/components/auth/UserNotFound.tsx
@@ -0,0 +1,60 @@
+import { IconStack3, IconUserOff } from '@tabler/icons-react'
+import { NavLink, useNavigate } from 'react-router'
+import { DarkModeToggle } from '../globals/DarkModeToggle'
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from '../ui/empty'
+import { Button } from '../ui/button'
+
+export const UserNotFound = () => {
+  const navigate = useNavigate()
+
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <div className="flex gap-6 justify-between">
+          <NavLink
+            to="/"
+            className="flex items-center gap-2 self-center font-bold"
+          >
+            <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+              <IconStack3 className="size-4" />
+            </div>
+            PendingList
+          </NavLink>
+          <DarkModeToggle />
+        </div>
+        <Empty className="dark:bg-stone-900 bg-white border border-solid">
+          <EmptyHeader>
+            <EmptyMedia variant="icon">
+              <IconUserOff />
+            </EmptyMedia>
+            <EmptyTitle>Usuario no autorizado</EmptyTitle>
+            <EmptyDescription>
+              Aún no has iniciado sesión. Empieza creando tu cuenta o inicia
+              sesión.
+            </EmptyDescription>
+          </EmptyHeader>
+          <EmptyContent className="flex-row justify-center gap-2">
+            <Button onClick={() => navigate('/auth', { viewTransition: true })}>
+              Iniciar sesión
+            </Button>
+            <Button
+              variant="outline"
+              onClick={() =>
+                navigate('/auth/register', { viewTransition: true })
+              }
+            >
+              Crear cuenta
+            </Button>
+          </EmptyContent>
+        </Empty>
+      </div>
+    </div>
+  )
+}
--- packages/frontend/src/pages/dashboard/AppLayout.tsx
+++ packages/frontend/src/pages/dashboard/AppLayout.tsx
@@ -1,21 +1,12 @@
-import { Button } from '@/components/ui/button'
+import { UserNotFound } from '@/components/auth/UserNotFound'
 import { useUserData } from '@/contexts/user-data/useUserData'
-import { useNavigate } from 'react-router'
 
 export const AppLayout = () => {
   const { userData } = useUserData()
 
-  const navigate = useNavigate()
-
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
-      {!userData ? (
-        <Button onClick={() => navigate('/auth', { viewTransition: true })}>
-          Iniciar sesión
-        </Button>
-      ) : (
-        <p>{JSON.stringify(userData)}</p>
-      )}
+      {!userData ? <UserNotFound /> : <p>{JSON.stringify(userData)}</p>}
     </div>
   )
 }
